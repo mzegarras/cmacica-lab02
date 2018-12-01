5c237b0d40691f2b1e5c1aca88b430ff32e3adf6
Manuel <mzegarra@gmail.com>
2018-12-01 05:56:09 -0500

    Primera version

---
+++ src/main/java/pe/cmacica/labs/Labs02Application.java
@@ -0,0 +1,14 @@
+package pe.cmacica.labs;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ * @author Manuel Zegarra
+ */
+@SpringBootApplication
+public class Labs02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labs02Application.class, args);
+    }
+}
